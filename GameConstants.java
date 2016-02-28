fc21349b844f3f37f2b51c8e776cbb977d23cfad
last-ent <last_ent@outlook.com>
2016-02-29 01:02:02 +0530

    Update GameConstants.java

--- GameConstants.java
+++ GameConstants.java
@@ -4,6 +4,9 @@ public class GameConstants {
     public static final float SECOND = 1f;
     public static final float BLOCK_SIDE_SIZE = 24f;
 
+    private static final float WORLD_WIDTH = 480;
+    private static final float WORLD_HEIGHT = 640;
+
     public static final int GRID_ROWS = 22;
     public static final int GRID_COLUMNS = 10;
     public static final float WORLD_HEIGHT = (GRID_ROWS + 3) * BLOCK_SIDE_SIZE;
