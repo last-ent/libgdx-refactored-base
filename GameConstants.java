74ac3fc72b63e970bc0dca1484b670414f632d35
last-ent <last_ent@outlook.com>
2016-02-29 01:01:15 +0530

    Create GameConstants.java

---
+++ GameConstants.java
@@ -0,0 +1,11 @@
+
+
+public class GameConstants {
+    public static final float SECOND = 1f;
+    public static final float BLOCK_SIDE_SIZE = 24f;
+
+    public static final int GRID_ROWS = 22;
+    public static final int GRID_COLUMNS = 10;
+    public static final float WORLD_HEIGHT = (GRID_ROWS + 3) * BLOCK_SIDE_SIZE;
+    public static final float WORLD_WIDTH = (GRID_COLUMNS + 5) * BLOCK_SIDE_SIZE;
+}
